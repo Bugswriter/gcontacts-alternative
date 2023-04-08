0fd9cd9081247129817e353c183441464936b2e0
Amanse <amansetiarjp@gmail.com>
2023-04-08 17:05:07 +0530

    Complete rewrite in rust

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+use rusqlite::Connection;
 use tide::prelude::*;
-use tide::{Body, Request, Result};
+use tide::{Body, Request};
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Contact {
     name: String,
-    phone: u64,
+    phone: i64,
 }
 
 #[derive(Deserialize)]
@@ -17,12 +18,20 @@ async fn main() -> tide::Result<()> {
     femme::start();
     let mut app = tide::new();
     app.at("/search").get(|req: Request<()>| async move {
+        let con = Connection::open("./site.db")?;
         let q: Params = req.query()?;
-        println!("{}", q.q);
-        let contacts: Vec<Contact> = vec![Contact {
-            name: "Bruj".to_string(),
-            phone: 9988497716,
-        }];
+        let mut contacts: Vec<Contact> = vec![];
+        let mut stmt = con.prepare(format!("SELECT * FROM phonebook where `names` LIKE '%{}%'", q.q).as_str())?;
+        let person_iter = stmt.query_map([], |row| {
+            let phone: String = row.get(1)?;
+            Ok(Contact {
+                name: row.get(0)?,
+                phone: phone.parse().unwrap(),
+            })
+        })?;
+        for person in person_iter {
+            contacts.push(person.unwrap());
+        }
         Body::from_json(&contacts)
     });
     app.at("/").serve_file("static/home.html")?;
